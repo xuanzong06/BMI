c811861f481317574a8327bbc0d04f046b85cdb0
yge <xuanzong06@gmail.com>
2019-02-12 21:18:04 +0800

    v4 增加了開啟相機的功能，但是拍出來會很模糊

--- app/src/main/java/com/example/bmi/MainActivity.java
+++ app/src/main/java/com/example/bmi/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.bmi;
 
 import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,23 +10,41 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private Button btn1;
+    private Button cambtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        btn1 = findViewById(R.id.gopage2btn);
+        FindView();
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 go();
             }
         });
+        cambtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cameraup();
+            }
+        });
+    }
+
+    private void FindView() {
+
+        cambtn = findViewById(R.id.cambtn);
+        btn1 = findViewById(R.id.gopage2btn);
+
     }
 
     public void go(){
         Intent nextpage = new Intent(this, CalculationMBI.class);
         startActivity(nextpage);
+    }
+
+    public void cameraup(){
+        Intent camerago = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivity(camerago);
     }
 }
